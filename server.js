5cdb4dccc7c7df389e255d8763168b7448419f15
Saad Khan <saad@Saads-MacBook-Pro.local>
2020-08-18 12:51:39 +0800

    lesson one done - created bootcamps routes

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const dotenv = require("dotenv");
 
+// Routes files
 // Load env vars
 dotenv.config({ path: "./config/config.env" });
 
 const app = express();
+const bootcamps = require("./routes/bootcamps");
+
+// Mount routers
+app.use("/api/v1/bootcamps", bootcamps);
+
 const PORT = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.send("Hello from express");
+});
 
 app.listen(
   PORT,
