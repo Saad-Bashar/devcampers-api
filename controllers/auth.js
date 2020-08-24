b0dcc27ba0f2d85ee2abff78f186e3a6b33dba76
Saad Khan <saad@Saads-MacBook-Pro.local>
2020-08-24 16:19:40 +0800

    login done

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const ErorResponse = require("../utils/ErrorResponse");
 const User = require("../models/User");
+const ErrorResponse = require("../utils/ErrorResponse");
 
 exports.register = async (req, res, next) => {
   try {
@@ -11,6 +12,40 @@ exports.register = async (req, res, next) => {
       password,
       role,
     });
+
+    const token = user.getSignedJwtToken();
+
+    res.status(200).json({ success: true, user, token });
+  } catch (error) {
+    // res.status(400).json({ success: false });
+    next(error);
+  }
+};
+
+exports.login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    // validate email and
+    if (!email || !password) {
+      return next(
+        new ErrorResponse("Please provide an email and password", 400)
+      );
+    }
+
+    // check for user
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      return next(new ErrorResponse("Invalid credentials", 400));
+    }
+
+    // check password
+    const isMatch = await user.matchPassword(password);
+
+    if (!isMatch) {
+      return next(new ErrorResponse("Invalid credentials", 400));
+    }
 
     const token = user.getSignedJwtToken();
 
